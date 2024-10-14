c92bf34d019571f1023689d9a1b2e617b61b13c1
anakha123 <anakhasanthoshtvpm@gmail.com>
2024-10-14 14:33:54 +0530

    err

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router(); 
-const userController= require('../controllers/user/userControllers');
+const userController= require('../controllers/user/usercontrollers');
 
 router.get('/pageNotFound',userController.pageNotFound);
 router.get('/',userController.loadHome);
