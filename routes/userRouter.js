22e5b79f5020a61e3d7cd70952e51c3811c369b5
Anakha Santhosh <108932100+anakha12@users.noreply.github.com>
2024-12-11 20:57:50 +0530

    Update userRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router(); 
-const userController= require('../controllers/user/usercontrollers');
+const userController= require('../controllers/user/userControllers');
 const profileController= require('../controllers/user/profileController');
 const orderController= require('../controllers/user/orderController');
 const cartController= require('../controllers/user/cartController');
